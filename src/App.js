83d728e404a9f70978b27596c4954ba08aaf1ece
Frempong <samankwaa.frempong@gmail.com>
2023-04-09 19:01:32 +0000

    Add HeroCarousel to Hero.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { CgMenuGridO } from 'react-icons/cg';
 import { useDispatch } from 'react-redux';
 import Navbar from './Components/Navbar';
 import { toggleNav } from './redux/navbar/NavbarSlice';
+import Hero from './Components/Hero';
 
 function App() {
   const dispatch = useDispatch();
@@ -10,8 +11,8 @@ function App() {
   return (
     <div className="App">
       <CgMenuGridO className="menu-icon" onClick={() => dispatch(toggleNav())} />
-
       <Navbar />
+      <Hero />
     </div>
   );
 }
