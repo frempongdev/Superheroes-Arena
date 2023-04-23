db321a0f221572eeb6aac9cc463564d27cf9bbb6
Frempong <samankwaa.frempong@gmail.com>
2023-04-23 23:07:18 +0000

    Add seasrch slice to component and implement searchHero fetch action

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,16 +1,23 @@
-import { useState } from 'react';
 import '../style/Search.css';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react'; // import useState hook
+import { searchHero } from '../redux/navbar/SearchSlice';
 
 const Search = () => {
-  const [input, setInput] = useState('');
+  const [inputValue, setInputValue] = useState(''); // use state hook to store input value
+  const dispatch = useDispatch();
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value); // update input value in state
+    dispatch(searchHero(e.target.value)); // dispatch searchHero with input value
+  };
 
   return (
     <div className="main">
       <div className="search-box">
-        <input type="text" className="input" placeholder="Search your SuperHero..." value={input} onChange={(e) => setInput(e.target.value)} />
-
+        <input type="text" className="input" placeholder="Search your SuperHero..." value={inputValue} onChange={handleInputChange} />
       </div>
-      <div className="results">{input}</div>
+      <div className="results">No</div>
     </div>
   );
 };
--- src/redux/navbar/SearchSlice.js
+++ src/redux/navbar/SearchSlice.js
@@ -1,15 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   searched: [],
 };
 
-// const searchHero
+export const searchHero = createAsyncThunk('search/searchHero', async (input) => {
+  const url = `https://superheroapi.com/api/10223015174750967/search/${input}`;
+  const response = await fetch(url, { mode: 'cors' });
+  const data = await response.json();
+  console.log(data);
+  return data;
+});
 
 const searchSlice = createSlice({
-  name: 'searched',
+  name: 'search',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(searchHero.fulfilled, (state, action) => ({ ...state, searched: action.payload }));
+  },
+
 });
 
 export default searchSlice.reducer;
