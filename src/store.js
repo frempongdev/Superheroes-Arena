9f13438e981b3ba283cb7167cabdc1671cafc79c
Frempong <samankwaa.frempong@gmail.com>
2023-08-07 23:58:45 +0000

    Add seachReducer to store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import navReducer from './redux/navbar/NavbarSlice';
+import searchReducer from './redux/navbar/SearchSlice';
 
 const store = configureStore({
   reducer: {
     nav: navReducer,
+    search: searchReducer,
   },
 });
 
