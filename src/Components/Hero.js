bad458ac618dcbc6786a628b4da778e1898fedbc
Frempong <samankwaa.frempong@gmail.com>
2023-08-09 02:12:31 +0000

    Add searchModal to Hero Page

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,9 +1,11 @@
 import '../style/Hero.css';
 import HeroCarousel from './HeroCarousel';
+import SearchModal from './SearchModal';
 
 const Hero = () => (
   <div className="hero">
     <div className="down-box">
+      <SearchModal />
       <HeroCarousel />
     </div>
   </div>
