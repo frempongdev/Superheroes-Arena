7c97fc93bbdc0345285748f32662c03cd5c67292
Frempong <samankwaa.frempong@gmail.com>
2023-08-09 02:11:24 +0000

    Remove search results from search component

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,18 +1,17 @@
 import '../style/Search.css';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { searchHero } from '../redux/navbar/SearchSlice';
 
 const Search = () => {
   const [inputValue, setInputValue] = useState(''); // use state hook to store input value
 
-  const { searched } = useSelector((state) => state.search);
-
   const dispatch = useDispatch();
 
   const handleInputSubmit = (e) => {
     e.preventDefault();
     dispatch(searchHero(inputValue)); // dispatch searchHero with input value
+    setInputValue('');
   };
 
   return (
@@ -21,16 +20,6 @@ const Search = () => {
         <input type="text" className="input" placeholder="Search your SuperHero..." value={inputValue} onChange={(e) => { setInputValue(e.target.value); }} />
         <input type="submit" className="submit" onClick={handleInputSubmit} />
       </form>
-      <div className="results">
-        {
-          searched.map((oneHero) => (
-            <div key={oneHero.id}>
-              <p>{oneHero.name}</p>
-              <img src={oneHero.images.xs} alt="hero-pic" />
-            </div>
-          ))
-        }
-      </div>
     </div>
   );
 };
