ddf69564cf63a7a2ff73f25428767cc3a14bfd35
Frempong <samankwaa.frempong@gmail.com>
2023-08-15 13:24:37 +0000

    Update Search Popup to display search results

--- src/Components/SearchModal.js
+++ src/Components/SearchModal.js
@@ -1,37 +1,39 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import '../style/SearchModal.css';
 import React from 'react';
 import { IoMdCloseCircle } from 'react-icons/io';
+import { Link } from 'react-router-dom';
+import { clearSearch } from '../redux/navbar/SearchSlice';
 
 const SearchModal = () => {
   const { searched } = useSelector((state) => state.search);
+  const dispatch = useDispatch();
+
+  const handleCloseSearch = () => {
+    dispatch(clearSearch());
+  };
 
   return (
-    <>
-      {searched.length !== 0 && (
-
-      <div className="modal-overlay">
-        <div className="sea">
-          <div className="results">
-            {
+    <div className="modal-overlay">
+      <div className="sea">
+        <div className="results">
+          {
             searched.map((oneHero) => (
-              <div className="hero-box" key={oneHero.id}>
-                <div className="hero-desc" style={{ backgroundImage: `url(${oneHero.images.sm})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
-                  <p>{oneHero.name}</p>
-                  <span>&#9733;</span>
-                  <span>{Math.floor(Math.random() * 6)}</span>
+              <Link to={`/hero/${oneHero.id}`} key={oneHero.id} className="hero-link">
+                <div className="hero-box">
+                  <div className="hero-desc" style={{ backgroundImage: `url(${oneHero.images.sm})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+                    <p>{oneHero.name}</p>
+                    <span>&#9733;</span>
+                    <span>{Math.floor(Math.random() * 6)}</span>
+                  </div>
                 </div>
-              </div>
+              </Link>
             ))
             }
-          </div>
         </div>
-        <IoMdCloseCircle className="close-search" onClick={() => { searched.length = 0; }} />
       </div>
-
-      )}
-    </>
-
+      <IoMdCloseCircle className="close-search" onClick={handleCloseSearch} />
+    </div>
   );
 };
 
