89366cc02268aea3bce58f5fd08f97287b9fad11
Frempong <samankwaa.frempong@gmail.com>
2023-08-15 14:55:40 +0000

    Add HeroSlice to manage single hero API calls

---
+++ src/redux/navbar/HeroSlice.js
@@ -0,0 +1,30 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const fetchHero = createAsyncThunk('hero/fetchHero', async (id) => {
+  const url = `https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${id}.json`;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  // console.log(url);
+  return data;
+});
+
+const initialState = {
+  heroDetails: {},
+  isLoading: false,
+};
+
+const heroSlice = createSlice({
+  name: 'hero',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchHero.pending, (state) => ({ ...state, isLoading: true }))
+      .addCase(fetchHero.fulfilled, (state, { payload }) => (
+        { ...state, isLoading: false, heroDetails: payload }))
+      .addCase(fetchHero.rejected, (state) => ({ ...state, isLoading: true }));
+  },
+});
+
+export default heroSlice.reducer;
