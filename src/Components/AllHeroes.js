1627afd89a3423d4a0d09ba79d880284679f85f6
Frempong <samankwaa.frempong@gmail.com>
2023-08-28 20:42:39 +0000

    Replace searched state property with allHerores to fix bug of showing searches after all heroes have been displayed

--- src/Components/AllHeroes.js
+++ src/Components/AllHeroes.js
@@ -1,21 +1,21 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { searchHero } from '../redux/navbar/SearchSlice';
+import { fetchAllHeroes } from '../redux/navbar/SearchSlice';
 import '../style/AllHeroes.css';
 
 const AllHeroes = () => {
-  const { searched } = useSelector((state) => state.search);
+  const { allHeroes } = useSelector((state) => state.search);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(searchHero(''));
+    dispatch(fetchAllHeroes());
   }, [dispatch]);
 
   return (
     <div className="all-heroes-wrapper">
       {
-            searched.map((oneHero) => (
+            allHeroes.map((oneHero) => (
               <Link to={`/hero/${oneHero.id}`} key={oneHero.id} className="hero-link">
                 <div className="hero-box">
                   <div className="hero-desc" style={{ backgroundImage: `url(${oneHero.images.sm})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
